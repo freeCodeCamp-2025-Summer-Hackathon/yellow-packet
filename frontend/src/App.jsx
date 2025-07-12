4fd3c5e5f08d90c775ad8bb6830a039d13635820
Quang Vinh Dang <dqvinh101@gmail.com>
2025-07-12 16:12:02 -0700

    Update branch commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import LandingPage from './pages/LandingPage';
 import BrowsePets from './pages/BrowsePets';
 import LoginPage from './pages/LoginPage';
 import PetProfile from './pages/PetProfile';
+import Signup from "./pages/Signup";
+import SignIn from "./pages/SignIn";
 
 function App() {
 	const [user, setUser] = useState(null);
@@ -14,6 +16,8 @@ function App() {
 				<Route path="/browse" element={<BrowsePets user={user} setUser={setUser} />} />
 				<Route path="/login" element={<LoginPage user={user} setUser={setUser} />} />
 				<Route path="/pet-profile/:petId" element={<PetProfile user={user} setUser={setUser} />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/signin" element={<SignIn />} />
 			</Routes>
 		</Router>
 	)
