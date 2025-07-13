e0454e11de99d8241cdd6d3a6699ebe1b9bacdf4
Adam O'Camb <18337439+adamocamb@users.noreply.github.com>
2025-07-13 13:14:09 -0700

    Implement PetCard component.
    Make and style the PetCard component. The home
    page just links to the petcard for testing. This
    will be changed back to normal before merging.

--- frontend/src/components/PetCard.jsx
+++ frontend/src/components/PetCard.jsx
@@ -1,9 +1,23 @@
-import React from "react"
- 
-export default function PetCard(){
+import React from "react";
+import "../styles/PetCard.css";
+
+export default function PetCard() {
     return ( 
     <>
-        {/* This is a pet card component representing one individual pet card */}
+        <div className="card">
+            <div className="image">
+                <img src="https://picsum.photos/200" alt="Image of pet" className="petImg"/>
+            </div>
+            <div className="petName">
+                <span className="fav"></span>
+                <h2 className="name">Pet Name</h2>
+                <button type="button" className="fav">♡</button>
+            </div>
+            <div className="petInfo">
+                <h3 className="petAge">Pet Age and Type</h3>
+                <p className="petDesc">Pet Gender and Location</p>
+            </div>
+        </div>
     </>
     );
 };
