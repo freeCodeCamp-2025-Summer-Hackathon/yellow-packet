5eb8839f535344317aecbe8891c434dce1f68d2a
Adam O'Camb <18337439+adamocamb@users.noreply.github.com>
2025-07-14 10:39:43 -0700

    implement the pet card (#71)
    * Implement PetCard component.
    
    Make and style the PetCard component. The home
    page just links to the petcard for testing. This
    will be changed back to normal before merging.
    
    * Update the PetCard with props
    
    Add props to the PetCard to pass in user entered
    information. The favorite button is not yet
    functional.
    
    * Update PetCard.css with Gio's changes
    
    Update PetCard.css to be more responsive to
    different image sizes.

--- frontend/src/components/PetCard.jsx
+++ frontend/src/components/PetCard.jsx
@@ -1,9 +1,32 @@
-import React from "react"
- 
-export default function PetCard(){
+import React from "react";
+import "../styles/PetCard.css";
+
+let isFav = false;
+
+function btnFunction () {
+    isFav = !isFav;
+    console.log(isFav);
+
+    return isFav;
+}
+
+export default function PetCard({name, age, gender, location, type, pic}) {
     return ( 
     <>
-        {/* This is a pet card component representing one individual pet card */}
+        <div className="card">
+            <div className="image">
+                <img src={pic} alt="Image of pet" className="petImg"/>
+            </div>
+            <div className="petName">
+                <span className="fav"></span>
+                <h2 className="name">{name}</h2>
+                <button onClick={btnFunction} type="button" className="fav">♥️🩶</button>
+            </div>
+            <div className="petInfo">
+                <h3 className="petAge">{age}-year-old {type}</h3>
+                <p className="petDesc">{gender} - {location}</p>
+            </div>
+        </div>
     </>
     );
 };
