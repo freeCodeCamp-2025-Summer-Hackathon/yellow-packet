97882f2db8693888da8578a782d952c408875294
UhOhGio <gioehrig@gmail.com>
2025-07-22 14:48:35 -0700

    undoing unnecessary changes to PetCard

--- frontend/src/components/PetCard.jsx
+++ frontend/src/components/PetCard.jsx
@@ -1,12 +1,12 @@
 import "../styles/PetCard.css";
 
-export default function PetCard({ id, name, age, gender, location, type, pics, isFav, onToggleFav, handleClick}) {
+export default function PetCard({ name, age, gender, location, type, pics, isFav, onToggleFav, handleClick}) {
 
     
 
     return ( 
     <>
-        <div className="card" onClick={() => handleClick({ id, name })}>
+        <div className="card" onClick={handleClick}>
             <div className="image">
                 <img src={pics[0]} alt="Image of pet" className="petImg"/>
             </div>
