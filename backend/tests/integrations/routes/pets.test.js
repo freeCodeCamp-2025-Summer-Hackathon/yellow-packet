87d6a18c6af5f6a9118739eab7d1a5187ad1e5d6
Quang Vinh Dang <dqvinh101@gmail.com>
2025-08-05 21:05:51 -0700

    Finished intergration test

--- backend/tests/integrations/routes/pets.test.js
+++ backend/tests/integrations/routes/pets.test.js
@@ -1,15 +1,14 @@
-// import supertest from "supertest";
-// import app from "../../../server";
-// const requestWithSupertest = supertest(app);
+import supertest from "supertest";
+import app from "../../../server";
+const requestWithSupertest = supertest(app);
 
-// Intergration test
-// describe('GET "/"', () => {
-// 	test('GET "/" returns all pets', async () => {
-// 		const res = await requestWithSupertest.get("/api/pets");
-// 		expect(res.status).toEqual(200);
-// 		expect(res.type).toEqual(expect.stringContaining("json"));
-// 	});
-// });
+describe('GET "/"', () => {
+	test('GET "/" returns all pets', async () => {
+		const res = await requestWithSupertest.get("/api/pets");
+		expect(res.status).toEqual(200);
+		expect(res.type).toEqual(expect.stringContaining("json"));
+	});
+});
 
 // describe('GET "/:id"', () => {
 // 	test('GET "/:id" returns given pet', async () => {
